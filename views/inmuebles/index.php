39f26713bb6f2cd4df050c76320ab9be7caa0450
Javier Loring <jloringm@gmail.com>
2018-05-23 19:33:00 +0200

    trabajo instituto

--- views/inmuebles/index.php
+++ views/inmuebles/index.php
@@ -9,19 +9,28 @@ use yii\grid\GridView;
 
 $this->title = 'Inmuebles';
 $this->params['breadcrumbs'][] = $this->title;
+$js = <<<EOT
+    $('.interesado').click(function(event) {
+        tlf = $(this).parents('tr').find('.telefono'); // .first();
+        tlf.show();
+    });
+EOT;
+$this->registerJs($js);
 ?>
 <div class="inmuebles-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
     <?php echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <!-- <p>
-        <?= Html::a('Create Inmuebles', ['create'], ['class' => 'btn btn-success']) ?>
-    </p> -->
+    <p>
+        <?= Html::a('Crear Inmueble', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
+        // 'filterModel' => $searchModel,
         'columns' => [
             // ['class' => 'yii\grid\SerialColumn'],
             //
@@ -33,8 +42,29 @@ $this->params['breadcrumbs'][] = $this->title;
             'garaje:boolean',
             'trastero:boolean',
             // 'propietario_id',
+            [
+                'attribute' => 'propietario.telefono',
+                'content' => function($model, $key, $index, $column){
+                    return '<span class="telefono" style:"dispaly: none;">'
+                    . $model->propietario->telefono
+                    . '</span>';
+                },
+            ],
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'template' => '{view} {update} {delete} {interesado}',
+                'buttons' => [
+                                'interesado' => function ($url, $model, $key) {
+                        return Html::button('Interesado', [
+                            'class' => 'btn-xs btn-success interesado',
+                            // 'data' => [
+                            //     'telefono' => $model->propietario->telefono,
+                            // ],
+                        ]);
+                    },
+                ],
 
-            ['class' => 'yii\grid\ActionColumn'],
+            ],
         ],
     ]); ?>
 </div>
